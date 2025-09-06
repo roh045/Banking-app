80b1584c4c6957d37fd6fd1c0e62993fb52fc95b
Your Name <your.email@example.com>
2025-09-07 00:08:08 +0530

    backend

--- backend/src/main/java/com/mybank/bank/BankApiApplication.java
+++ backend/src/main/java/com/mybank/bank/BankApiApplication.java
@@ -2,9 +2,10 @@ package com.mybank.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BankApiApplication {
+public class BankApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BankApiApplication.class, args);
